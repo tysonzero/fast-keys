aade0a7e11324a37eb7b8eea775e3dde20e2230e
Tysonzero <danielspencersmith@gmail.com>
2013-09-03 21:35:30 -0600

    Setup JavaScript base code
    Creates an empty canvas to draw on

---
+++ script.js
@@ -0,0 +1,26 @@
+var game = {
+  //declaring canvas and ctx so they can be globally accessed
+	canvas:null,
+	ctx:null,
+  
+  //use dimensions identical to size of enclosing <div>
+	width:800,
+	height:600,
+	
+	setup:function() {
+    //get canvas created in html so it can be accessed
+		this.canvas = document.getElementById('game_canvas');
+    
+    //set canvas size equal to predefined dimensions
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+    
+    //create context to allow drawing onto the canvas
+		this.ctx = this.canvas.getContext('2d');
+	},
+}
+
+window.onload = function() {
+  //starts up canvas once the page finishes loading
+	game.setup();
+}
