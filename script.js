5c50d7605321e3cc58f2194c79d7091cd087ccc1
Tysonzero <danielspencersmith@gmail.com>
2013-09-05 10:11:17 -0600

    Convert some tabs to spaces

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 var game = {
   //declaring canvas and ctx so they can be globally accessed
-	canvas:null,
-	ctx:null,
+  canvas:null,
+  ctx:null,
   
   //use dimensions identical to size of enclosing <div>
-	width:800,
-	height:600,
+  width:800,
+  height:600,
 	
 	setup:function() {
     //get canvas created in html so it can be accessed
 		this.canvas = document.getElementById('game_canvas');
     
     //set canvas size equal to predefined dimensions
-		this.canvas.width = this.width;
-		this.canvas.height = this.height;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
     
     //create context to allow drawing onto the canvas
-		this.ctx = this.canvas.getContext('2d');
+    this.ctx = this.canvas.getContext('2d');
     
     //allows keyboard input
-		keyBoardInput.initialize();
+    keyBoardInput.initialize();
     
     //calls run() every 1/60th of a second
-		this.timer = setInterval(function(){game.run()}, 16 + 2/3);	
+    this.timer = setInterval(function(){game.run()}, 16 + 2/3);	
 	},
   
   //run every 1/60th of a second
   run:function() {
-		this.update();
-		this.draw();
+    this.update();
+    this.draw();
 	},
 	
   //logic
