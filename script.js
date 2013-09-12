0332625e16e015e991e803a2136bb32f2fe88e65
Tysonzero <danielspencersmith@gmail.com>
2013-09-12 09:41:32 -0600

    Add exit button to pause menu

--- script.js
+++ script.js
@@ -186,19 +186,21 @@ var pause = {
     //draw pause menu buttons
     game.ctx.fillStyle="#FF0000";
     game.ctx.fillRect(300, 170, 200, 60);
-    game.ctx.fillStyle="#FF0000";
     game.ctx.fillRect(300, 250, 200, 60);
+    game.ctx.fillRect(300, 330, 200, 60);
     
     //draw pause menu actions
     game.ctx.font="30px Arial";
     game.ctx.fillStyle="#000000";
     game.ctx.fillText("RESUME", 400, 200);
     game.ctx.fillText("RESTART", 400, 280);
+    game.ctx.fillText("EXIT", 400, 360);
     
     //draw pause menu keys
     game.ctx.font="15px Arial";
     game.ctx.fillText("press space", 400, 220);
     game.ctx.fillText("press backspace", 400, 300);
+    game.ctx.fillText("press escape", 400, 380);
   },
 }
 
