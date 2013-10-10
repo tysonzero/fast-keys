969ebc106e8346a1f8468e9fe3403324b24d78d6
Tysonzero <danielspencersmith@gmail.com>
2013-10-10 09:09:53 -0600

    Add comments

--- scripts/vars/mouseInput.js
+++ scripts/vars/mouseInput.js
@@ -1,13 +1,16 @@
 var mouseInput = {
+  //creates listener
   initialize:function() {
     game.canvas.addEventListener('mousedown', this.mouseDown, false);
   },
   
+  //called when mouse is clicked
   mouseDown:function(evt) {
     mousePos = mouseInput.getMousePos(evt);
     alert("X=" + mousePos.x + " Y=" + mousePos.y);
   },
   
+  //returns mouse position within canvas
   getMousePos:function(evt) {
     var rect = game.canvas.getBoundingClientRect();
     return {
