2984513957a74a3593b563f6b234c0651cc37ab5
Tysonzero <danielspencersmith@gmail.com>
2015-01-09 23:28:58 -0700

    Rename _bind_keys to bind_keys

--- scripts/vars/keyBoardInput.js
+++ scripts/vars/keyBoardInput.js
@@ -1,10 +1,10 @@
 var keyBoardInput = {
     //creates keyboard listener
     initialize: function () {
-        this._bind_keys();
+        this.bind_keys();
     },
 
-    _bind_keys: function () {
+    bind_keys: function () {
         //called whenever a key is pressed
         window.onkeydown = function (e) {
             if(e.keyCode === 13) {
